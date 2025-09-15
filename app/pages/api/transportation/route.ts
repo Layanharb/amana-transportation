c999b302c59d1e7bdf25daa3a283952b7482d0ac
Layan Harb <layanharb67@gmail.com>
2025-09-15 22:04:40 +0300

    Add project files

--- app/pages/api/transportation/route.ts
+++ app/pages/api/transportation/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import transportationData from '../../../data/transportation-data.json';
+import transportationData from '../../../../data/transportation-data.json';
 
 export async function GET() {
   try {
